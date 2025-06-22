9970fed881f23b9cf5b4a02a5454000503fcc763
Nigel Breslaw <nigel.breslaw@slint.dev>
2025-06-22 18:40:16 +0300

    Update privacy policy

--- docs/src/navigation.ts
+++ docs/src/navigation.ts
@@ -99,7 +99,6 @@ export const footerData = {
     // },
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
